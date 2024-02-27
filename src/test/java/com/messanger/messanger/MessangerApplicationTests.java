e4a1364a6168232a2b8a34a0e44d50fb0cba5f28
Albert <akaramean1@mail.ru>
2024-02-27 10:27:31 +0200

    Мой первый коммит

---
+++ src/test/java/com/messanger/messanger/MessangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.messanger.messanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessangerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
